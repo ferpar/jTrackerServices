284a29204fcbdc684c4d88d7fccd40d144554585
Fer <ferp@protonmail.ch>
2024-01-14 21:15:23 +0100

    handle cors for development purposes

--- src/auth.js
+++ src/auth.js
@@ -1,20 +1,23 @@
 const dotenv = require("dotenv");
 dotenv.config();
+const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const { 
-  loginController, 
+const {
+  loginController,
   refreshTokenController,
-  logoutController
- } = require("./controllers/auth");
+  logoutController,
+} = require("./controllers/auth");
 
 const PORT = process.env.AUTH_PORT;
 const app = express();
 
+app.use(cors());
+
 app.use(bodyParser.json());
 
-app.post("/login", loginController );
+app.post("/login", loginController);
 
 app.post("/token", refreshTokenController);
 
